23611861b4c94bbf726867f32f98599c42a607c0
Gabriel Wu <wuzihua@pku.edu.cn>
2020-09-18 23:57:27 +0800

    test: should be fixed

--- src/LBLRTM.jl
+++ src/LBLRTM.jl
@@ -3,7 +3,7 @@ module LBLRTM
 using Unitful
 using PhysicalConstants.CODATA2018: h, ħ, k_B, c_0, π, g_n
 using Printf
-export run_lnfl, run_lblrtm
+export run_lnfl, run_lblrtm, planck, radiance_to_bt
 
 # Constants
 include("constants.jl")
